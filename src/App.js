579d7ed9e48005a062f47be53a346db6d5743bf6
achyuthask <96536973+achyuthask@users.noreply.github.com>
2023-04-28 22:51:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import React from 'react';
+
+
+const App=()=>{ 
+ 
+
+ 
+ const  para1=React.createElement('p','br' ,
+  [], 
+  
+ 
+ `Lorem ipsum dolor sit amet lContrary to popular belief, Lorem Ipsum is not simply random text.
+  It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+   Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin 
+   words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, 
+   discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus 
+   Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature 
+   from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+   looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of 
+   the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
+   "de Finibus  Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin 
+   literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,
+    looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in
+     classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus `
   );
-}
+
+ 
+return(
+<>
+<div className='App'>
+
+{para1}
+
+
+</div>
+</>
+);
+
+};
+
 
 export default App;
